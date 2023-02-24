832979333460e9c03c36adba0dd944c50b0e0691
yunheskaa <yunheska.duque@ext.softtek.com>
2023-02-24 09:55:39 -0300

    primer commit

--- src/app/usuarios/usuarios-routing.module.ts
+++ src/app/usuarios/usuarios-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
